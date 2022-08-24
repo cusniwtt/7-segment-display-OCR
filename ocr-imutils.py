7bd38d83edc198a764211887dd976c3f4f40204f
Wayu Ragwongsiri <windywayu2000@gmail.com>
2022-08-24 16:16:27 +0700

    Update ocr-imutils.py

--- ocr-imutils.py
+++ ocr-imutils.py
@@ -60,8 +60,76 @@ def plot(img):
 	plt.title("Histogram Showing Pixel Intensity And Corresponding Number Of Pixels", color='crimson')
 	plt.show()
 
+# find contours in the thresholded image, then initialize the
+# digit contours lists
+def contouring(img):
+	cnts = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+	digitCnts = []
+	# loop over the digit area candidates
+	for c in cnts:
+		# compute the bounding box of the contour
+		(x, y, w, h) = cv2.boundingRect(c)
+		# if the contour is sufficiently large, it must be a digit
+		if w >= 30 and (h >=80  and h <= 115):
+			rec = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 2)
+			digitCnts.append(c)
+	cv2.imshow('Contour', rec)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
 
-#Read the image
+	# sort the contours from left-to-right, then initialize the
+	# actual digits themselves
+	digitCnts = contours.sort_contours(digitCnts, method="left-to-right")[0]
+	digits = []
+
+	# loop over each of the digits
+	for c in digitCnts:
+		# extract the digit ROI
+		(x, y, w, h) = cv2.boundingRect(c)
+		roi = img[y:y + h, x:x + w]
+		# compute the width and height of each of the 7 segments
+		# we are going to examine
+		(roiH, roiW) = roi.shape
+		(dW, dH) = (int(roiW * 0.25), int(roiH * 0.15))
+		dHC = int(roiH * 0.05)
+		# define the set of 7 segments
+		segments = [
+			((0, 0), (w, dH)),	# top
+			((0, 0), (dW, h // 2)),	# top-left
+			((w - dW, 0), (w, h // 2)),	# top-right
+			((0, (h // 2) - dHC) , (w, (h // 2) + dHC)), # center
+			((0, h // 2), (dW, h)),	# bottom-left
+			((w - dW, h // 2), (w, h)),	# bottom-right
+			((0, h - dH), (w, h))	# bottom
+		]
+		on = [0] * len(segments)
+		# loop over the segments
+		for (i, ((xA, yA), (xB, yB))) in enumerate(segments):
+			# extract the segment ROI, count the total number of
+			# thresholded pixels in the segment, and then compute
+			# the area of the segment
+			segROI = roi[yA:yB, xA:xB]
+			total = cv2.countNonZero(segROI)
+			area = (xB - xA) * (yB - yA)
+			# if the total number of non-zero pixels is greater than
+			# 50% of the area, mark the segment as "on"
+			if total / float(area) > 0.5:
+				on[i]= 1
+		# lookup the digit and draw it on the image
+		digit = DIGITS_LOOKUP[tuple(on)]
+		digits.append(digit)
+		cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
+		cv2.putText(img, str(digit), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
+
+	# display the digits
+	print(u"{}{}.{}".format(*digits))
+	cv2.imshow("Input", img)
+	cv2.imshow("Output", img)
+	cv2.waitKey(0)
+
+# MAIN
+# Read the image
 img = cv2.imread('Image Test/t01.png', 0)
 img = imutils.resize(img, height=244, width=244)
 print(img.shape)
@@ -69,71 +137,8 @@ cv2.imshow('Original', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# find contours in the thresholded image, then initialize the
-# digit contours lists
-cnts = cv2.findContours(img_plot.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-digitCnts = []
-# loop over the digit area candidates
-for c in cnts:
-    # compute the bounding box of the contour
-    (x, y, w, h) = cv2.boundingRect(c)
-	# if the contour is sufficiently large, it must be a digit
-    if w >= 30 and (h >=80  and h <= 115):
-        rec = cv2.rectangle(img_plot, (x, y), (x + w, y + h), (255, 255, 255), 2)
-        digitCnts.append(c)
-cv2.imshow('Contour', rec)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+img = contrast_stretching(img)
+img = thresholding(img)
+img = opening(img)
+img = contouring(img)
 
-# sort the contours from left-to-right, then initialize the
-# actual digits themselves
-digitCnts = contours.sort_contours(digitCnts, method="left-to-right")[0]
-digits = []
-
-# loop over each of the digits
-for c in digitCnts:
-	# extract the digit ROI
-	(x, y, w, h) = cv2.boundingRect(c)
-	roi = img_plot[y:y + h, x:x + w]
-	# compute the width and height of each of the 7 segments
-	# we are going to examine
-	(roiH, roiW) = roi.shape
-	(dW, dH) = (int(roiW * 0.25), int(roiH * 0.15))
-	dHC = int(roiH * 0.05)
-	# define the set of 7 segments
-	segments = [
-		((0, 0), (w, dH)),	# top
-		((0, 0), (dW, h // 2)),	# top-left
-		((w - dW, 0), (w, h // 2)),	# top-right
-		((0, (h // 2) - dHC) , (w, (h // 2) + dHC)), # center
-		((0, h // 2), (dW, h)),	# bottom-left
-		((w - dW, h // 2), (w, h)),	# bottom-right
-		((0, h - dH), (w, h))	# bottom
-	]
-	on = [0] * len(segments)
-
-    # loop over the segments
-	for (i, ((xA, yA), (xB, yB))) in enumerate(segments):
-		# extract the segment ROI, count the total number of
-		# thresholded pixels in the segment, and then compute
-		# the area of the segment
-		segROI = roi[yA:yB, xA:xB]
-		total = cv2.countNonZero(segROI)
-		area = (xB - xA) * (yB - yA)
-		# if the total number of non-zero pixels is greater than
-		# 50% of the area, mark the segment as "on"
-		if total / float(area) > 0.5:
-			on[i]= 1
-	# lookup the digit and draw it on the image
-	digit = DIGITS_LOOKUP[tuple(on)]
-	digits.append(digit)
-	cv2.rectangle(img_plot, (x, y), (x + w, y + h), (0, 255, 0), 1)
-	cv2.putText(img_plot, str(digit), (x - 10, y - 10),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
-
-# display the digits
-print(u"{}{}.{}".format(*digits))
-cv2.imshow("Input", img)
-cv2.imshow("Output", img_plot)
-cv2.waitKey(0)
