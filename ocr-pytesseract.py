911577b8c2a6cada7ae79a2b272b5320bce21a3a
Wayu Ragwongsiri <windywayu2000@gmail.com>
2022-08-24 16:54:38 +0700

    Update ocr-pytesseract.py

--- ocr-pytesseract.py
+++ ocr-pytesseract.py
@@ -1,4 +1,5 @@
 # Get required libraries
+from re import I
 import cv2
 import numpy as np
 import pytesseract
@@ -6,7 +7,8 @@ import imutils
 
 # Define Functions
 def ocr_core(img):
-    text = pytesseract.image_to_string(img, config='--psm 6')
+    conf = '--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789'
+    text = pytesseract.image_to_string(img, config=conf)
     return text
 
 def get_grey_scale(img):
@@ -23,14 +25,19 @@ def thresholding(img):
     output = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
     return output
 
-def dilating(img):
+def opening(img):
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
     output = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
     return output
 
+def dilating(img):
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
+    output = cv2.dilate(img, kernel, iterations=1)
+    return output
+
 # Main steps
 
-image = cv2.imread('Image Test/t02.png')
+image = cv2.imread('Image Test/t01.png')
 image = imutils.resize(image, height=244, width=244)
 cv2.imshow('Original', image)
 cv2.waitKey(0)
@@ -39,6 +46,7 @@ cv2.destroyAllWindows()
 image = get_grey_scale(image)
 image = rescale(image, scale_percent=50)
 image = thresholding(image)
+image = opening(image)
 image = dilating(image)
 cv2.imshow('Preprocessing', image)
 cv2.waitKey(0)
